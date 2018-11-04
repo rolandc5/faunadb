d1b85ea1b72873ed503a4cc8d0f3e6bcbc22b22e
Roland <rolandcanuto@outlook.com>
2018-11-03 20:03:14 -0700

    first commit and push to faunadb

--- functions/components.js
+++ functions/components.js
@@ -3,5 +3,9 @@ const dot = require('dotenv').config();
 const faunadb = require('faunadb');
 const client = new faunadb.Client({ secret: process.env.FAUNADB_KEY })
 
-console.log(client);
-console.log(dot);
+exports.handler = function (event, context, callback) {
+    callback(null, {
+      statusCode: 200,
+      body: "Hello, World"
+    });
+  };
